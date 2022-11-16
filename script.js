0e81c8f9893938b376db64409c71fb75ac9d0c4a
SDeGraff <114950127+SDeGraff@users.noreply.github.com>
2022-11-16 13:43:49 -0600

    Javascript Start function

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ var startBtn = document.querySelector("#start");
 var initialsEl = document.querySelector("#initials");
 var feedbackEl = document.querySelector("#feedback");
 
+// Quiz Variables
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+
+// Hide Screen
+function startQuiz() {
+    var startScreenEl = document.getElementById("intro");
+    startScreenEl.setAttribute("class", "hide");
+// Unhide
+    questionEl.removeAttribute("class");
+
+//Start Timer
+    timerId = setInterval(clockTick, 1000)
+
+//Show Time
+    timerEl.textContent = time;
+
+    getQuestion();
+}
