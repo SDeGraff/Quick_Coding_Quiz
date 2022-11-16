f3af83934c0d75b7b300edde15630c8aa54f1d88
SDeGraff <114950127+SDeGraff@users.noreply.github.com>
2022-11-16 13:03:06 -0600

    Javascrip Variables setup

---
+++ script.js
@@ -0,0 +1,8 @@
+var questionEl = document.querySelector("#questions");
+var timerEl = document.querySelector("#timer");
+var choicesEl = document.querySelector("#choices");
+var submitBtn = document.querySelector("#submit");
+var startBtn = document.querySelector("#start");
+var initialsEl = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#feedback");
+
