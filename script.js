2fde16837643acbe674f8fe139c7f3be40866775
SDeGraff <114950127+SDeGraff@users.noreply.github.com>
2022-11-17 12:44:55 -0600

    more js

--- script.js
+++ script.js
@@ -49,3 +49,57 @@ function getQuestion() {
     });
 }
 
+function getQuestion() {
+    // get a question
+    var currentQuestion = questions[currentQuestionIndex];
+  
+    var titleEl = document.getElementById("question-title");
+    titleEl.textContent = currentQuestion.title;
+  
+    choicesEl.innerHTML = "";
+  
+    currentQuestion.choices.forEach(function(choice, i) {
+      var choiceNode = document.createElement("button");
+      choiceNode.setAttribute("class", "choice");
+      choiceNode.setAttribute("value", choice);
+  
+      choiceNode.textContent = i + 1 + ". " + choice;
+  
+      choiceNode.onclick = questionClick;
+
+      choicesEl.appendChild(choiceNode);
+    });
+  }
+
+  function questionClick() {
+    if (this.value !== questions[currentQuestionIndex].answer) {
+      time -= 15;
+  
+      if (time < 0) {
+        time = 0;
+      }
+      timerEl.textContent = time;
+      feedbackEl.textContent = "Wrong!";
+      feedbackEl.style.color = "red";
+      feedbackEl.style.fontSize = "400%";
+    } else {
+      feedbackEl.textContent = "Correct!";
+      feedbackEl.style.color = "green";
+      feedbackEl.style.fontSize = "400%";
+    }
+  
+        feedbackEl.setAttribute("class", "feedback");
+    setTimeout(function() {
+      feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+  
+    currentQuestionIndex++;
+
+    if (currentQuestionIndex === questions.length) {
+      quizEnd();
+    } else {
+      getQuestion();
+    }
+  }
+
+  
